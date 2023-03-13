e8f56946e71548b9e7e7030399e21ed6edcad225
Leander Hutton <leander@one-button.org>
2023-03-13 16:11:49 -0400

    updated nvidia

--- fedora_desktop_setup.sh
+++ fedora_desktop_setup.sh
@@ -6,7 +6,7 @@ DEFAULT_RPMS="thunderbird nextcloud-client quassel-client tmux vim htop vlc cmus
 PHOTO_RPMS="darktable rawtherapee"
 GNOME_RPMS="gwenview ufraw" 
 KDE_RPMS="rhythmbox kate"
-NVIDIA_RPMS=""
+NVIDIA_RPMS="akmod-nvidia xorg-x11-drv-nvidia-cuda"
 ASTRONOMY_RPMS="stellarium skychart"
 # figure out what DE is running
 DESKTOP=$(echo $XDG_CURRENT_DESKTOP)
@@ -51,7 +51,6 @@ sudo bash -c 'cat > /usr/share/thumbnailers/ufraw.thumbnailer' <<EOF
 Exec=/usr/bin/ufraw-batch --embedded-image --out-type=png --size=%s %u --overwrite --silent --output=%o
 MimeType=image/x-3fr;image/x-adobe-dng;image/x-arw;image/x-bay;image/x-canon-cr2;image/x-canon-crw;image/x-cap;image/x-cr2;image/x-crw;image/x-dcr;image/x-dcraw;image/x-dcs;image/x-dng;image/x-drf;image/x-eip;image/x-erf;image/x-fff;image/x-fuji-raf;image/x-iiq;image/x-k25;image/x-kdc;image/x-mef;image/x-minolta-mrw;image/x-mos;image/x-mrw;image/x-nef;image/x-nikon-nef;image/x-nrw;image/x-olympus-orf;image/x-orf;image/x-panasonic-raw;image/x-pef;image/x-pentax-pef;image/x-ptx;image/x-pxn;image/x-r3d;image/x-raf;image/x-raw;image/x-rw2;image/x-rwl;image/x-rwz;image/x-sigma-x3f;image/x-sony-arw;image/x-sony-sr2;image/x-sony-srf;image/x-sr2;image/x-srf;image/x-x3f;image/x-panasonic-raw2;image/x-nikon-nrw;
 EOF
-
 else
 echo "NOT GNOME!"
 fi
